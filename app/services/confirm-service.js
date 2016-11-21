1575bb1a6d9384e4af23ced59d4e0843d33b43c7
Unknown <unknown@unknown.com>
2016-11-21 10:14:38 +0000

    improve confirm service

--- app/services/confirm-service.js
+++ app/services/confirm-service.js
@@ -9,6 +9,12 @@ export default Ember.Service.extend({
    */
 	session: Ember.inject.service('session'),
 
+  /*
+   * rest service
+   * @public
+   * @{service}
+   */
+  rest: Ember.inject.service('rest'),
 
   /*
    * AJAX call to confirm a trade
@@ -19,26 +25,10 @@ export default Ember.Service.extend({
    * @param {Object} callback - method to call to deal with a field update
    */
 	confirm: function(dealRef, position, size, callback) {
-		const session = this.get('session');
-    const apiHost = session.session.content.authenticated.apiHost;
-		let req = {};
-		req.url = `${apiHost}/confirms/${dealRef}`;
-		req.headers = {
-			"Content-Type": "application/json; charset=UTF-8",
-			"Accept": "application/json; charset=UTF-8",
-			"X-IG-API-KEY": session.session.content.authenticated.api,
-			"CST": session.session.content.authenticated.cstToken,
-			"X-SECURITY-TOKEN": session.session.content.authenticated.ssoToken,
-		};
-
-		Ember.$.ajax({
-			type: 'GET',
-			url: req.url,
-			data: null,
-			headers: req.headers,
-			async: false,
-		}).then(function(response, status, data) {
-			callback(response, position, size);
-		});
+    this.get('rest').doGet(`/confirms/${dealRef}`, null, {
+      'version': 1
+    }).then(function(response, status, data) {
+      callback(response, position, size);
+    });
 	}
 });
