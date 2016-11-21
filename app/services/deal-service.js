69015801b017a1a7af98d836aa5d162d6e45d0e4
Unknown <unknown@unknown.com>
2016-11-21 10:37:29 +0000

    deal service improvements

--- app/services/deal-service.js
+++ app/services/deal-service.js
@@ -25,22 +25,8 @@
     * @param {Object} callback - method to call when AJAX call returns
     */
    closePosition: function(position, size, callback) {
-     const session = this.get('session');
-     const apiHost = session.session.content.authenticated.apiHost;
      const direction = (position.direction === 'BUY') ? 'SELL' : 'BUY';
-     let req = {};
-     req.url = `${apiHost}/positions/otc`;
-     req.headers = {
-       "Content-Type": "application/json; charset=UTF-8",
-       "Accept": "application/json; charset=UTF-8",
-       "X-IG-API-KEY": session.session.content.authenticated.api,
-       "CST": session.session.content.authenticated.cstToken,
-       "X-SECURITY-TOKEN": session.session.content.authenticated.ssoToken,
-       "Version": 1,
-       "_method": "DELETE"
-     };
-
-     const bodyParams = {
+     const data = {
        dealId: position.dealId,
        epic: null,
        expiry: null,
@@ -51,14 +37,9 @@
        timeInForce: null,
        quoteId: null
      };
-     req.body = JSON.stringify(bodyParams);
-
-     Ember.$.ajax({
-       type: 'POST',
-       url: req.url,
-       data: req.body,
-       headers: req.headers,
-       async: false,
+     this.get('rest').doPost(`/positions/otc`, JSON.stringify(data), {
+       'version': 1,
+       "_method": "DELETE"
      }).then(function(response, status, data) {
        callback(response, position, size);
      });
@@ -71,77 +52,42 @@
     * @param {Object} callback - method to call when AJAX call returns
     */
    closeOrder: function(dealId, callback) {
-     const session = this.get('session');
-     const apiHost = session.session.content.authenticated.apiHost;
-     let req = {};
-     req.url = `${apiHost}/workingorders/otc/${dealId}`;
-     req.headers = {
-       "Content-Type": "application/json; charset=UTF-8",
-       "Accept": "application/json; charset=UTF-8",
-       "X-IG-API-KEY": session.session.content.authenticated.api,
-       "CST": session.session.content.authenticated.cstToken,
-       "X-SECURITY-TOKEN": session.session.content.authenticated.ssoToken,
-       "Version": 2,
+     this.get('rest').doPost(`/workingorders/otc/${dealId}`, null, {
+       'version': 2,
        "_method": "DELETE"
-     };
-
-     Ember.$.ajax({
-       type: 'DELETE',
-       url: req.url,
-       data: {},
-       headers: req.headers,
-       async: false,
      }).then(function(response, status, data) {
        callback(response);
      });
-  },
+   },
 
-  /*
-   * AJAX call to open a position
-   * @public
-   * @param {Object} dealParams - Variables used to create the position
-   * @param {Object} callback - method to call when AJAX call returns
-   */
+   /*
+    * AJAX call to open a position
+    * @public
+    * @param {Object} dealParams - Variables used to create the position
+    * @param {Object} callback - method to call when AJAX call returns
+    */
    openPosition(dealParams, callback) {
-     const session = this.get('session');
-     const apiHost = session.session.content.authenticated.apiHost;
-     let req = {};
-     req.url = `${apiHost}/positions/otc`;
-     req.headers = {
-       "Content-Type": "application/json; charset=UTF-8",
-       "Accept": "application/json; charset=UTF-8",
-       "X-IG-API-KEY": session.session.content.authenticated.api,
-       "CST": session.session.content.authenticated.cstToken,
-       "X-SECURITY-TOKEN": session.session.content.authenticated.ssoToken,
-       "Version": 2,
-     };
+     const data = {};
+     data["epic"] = dealParams.epic;
+     data["expiry"] = dealParams.expiry;
+     data["direction"] = dealParams.direction ? 'BUY' : 'SELL';
+     data["size"] = dealParams.size;
+     data["orderType"] = 'MARKET';
+     data["timeInForce"] = null;
+     data["level"] = null;
+     data["guaranteedStop"] = false;
+     data["stopLevel"] = null;
+     data["stopDistance"] = null;
+     data["trailingStop"] = false;
+     data["trailingStopIncrement"] = null;
+     data["forceOpen"] = false;
+     data["limitLevel"] = null;
+     data["limitDistance"] = null;
+     data["quoteId"] = null;
+     data["currencyCode"] = 'GBP';
 
-     const bodyParams = {};
-     bodyParams["epic"] = dealParams.epic;
-     bodyParams["expiry"] = dealParams.expiry;
-     bodyParams["direction"] = dealParams.direction ? 'BUY' : 'SELL';
-     bodyParams["size"] = dealParams.size;
-     bodyParams["orderType"] = 'MARKET';
-     bodyParams["timeInForce"] = null;
-     bodyParams["level"] = null;
-     bodyParams["guaranteedStop"] = false;
-     bodyParams["stopLevel"] = null;
-     bodyParams["stopDistance"] = null;
-     bodyParams["trailingStop"] = false;
-     bodyParams["trailingStopIncrement"] = null;
-     bodyParams["forceOpen"] = false;
-     bodyParams["limitLevel"] = null;
-     bodyParams["limitDistance"] = null;
-     bodyParams["quoteId"] = null;
-     bodyParams["currencyCode"] = 'GBP';
-     req.body = JSON.stringify(bodyParams);
-
-     Ember.$.ajax({
-       type: 'POST',
-       url: req.url,
-       data: req.body,
-       headers: req.headers,
-       async: false,
+     this.get('rest').doPost(`/positions/otc`, JSON.stringify(data), {
+       'version': 2
      }).then(function(response, status, data) {
        callback(response, status, data);
      });
@@ -154,43 +100,25 @@
     * @param {Object} callback - method to call when AJAX call returns
     */
    workingOrder(dealParams, callback) {
-     const session = this.get('session');
-     const apiHost = session.session.content.authenticated.apiHost;
-     let req = {};
-     req.url = `${apiHost}/workingorders/otc`;
-     req.headers = {
-       "Content-Type": "application/json; charset=UTF-8",
-       "Accept": "application/json; charset=UTF-8",
-       "X-IG-API-KEY": session.session.content.authenticated.api,
-       "CST": session.session.content.authenticated.cstToken,
-       "X-SECURITY-TOKEN": session.session.content.authenticated.ssoToken,
-       "Version": 2,
-     };
+     const data = {};
+     data["epic"] = dealParams.epic;
+     data["expiry"] = dealParams.expiry;
+     data["direction"] = dealParams.direction ? 'BUY' : 'SELL';
+     data["size"] = dealParams.size;
+     data["level"] = dealParams.level;
+     data["forceOpen"] = null;
+     data["type"] = 'STOP';
+     data["currencyCode"] = 'GBP';
+     data["timeInForce"] = dealParams.goodTill === 'Cancelled' ? 'GOOD_TILL_CANCELLED' : 'GOOD_TILL_DATE';
+     data["goodTillDate"] = dealParams.goodTillDate;
+     data["guaranteedStop"] = false;
+     data["stopLevel"] = null;
+     data["stopDistance"] = null;
+     data["limitLevel"] = null;
+     data["limitDistance"] = null;
 
-     const bodyParams = {};
-     bodyParams["epic"] = dealParams.epic;
-     bodyParams["expiry"] = dealParams.expiry;
-     bodyParams["direction"] = dealParams.direction ? 'BUY' : 'SELL';
-     bodyParams["size"] = dealParams.size;
-     bodyParams["level"] = dealParams.level;
-     bodyParams["forceOpen"] = null;
-     bodyParams["type"] = 'STOP';
-     bodyParams["currencyCode"] = 'GBP';
-     bodyParams["timeInForce"] = dealParams.goodTill === 'Cancelled' ? 'GOOD_TILL_CANCELLED' : 'GOOD_TILL_DATE';
-     bodyParams["goodTillDate"] = dealParams.goodTillDate;
-     bodyParams["guaranteedStop"] = false;
-     bodyParams["stopLevel"] = null;
-     bodyParams["stopDistance"] = null;
-     bodyParams["limitLevel"] = null;
-     bodyParams["limitDistance"] = null;
-     req.body = JSON.stringify(bodyParams);
-
-     Ember.$.ajax({
-       type: 'POST',
-       url: req.url,
-       data: req.body,
-       headers: req.headers,
-       async: false,
+     this.get('rest').doPost(`/workingorders/otc`, JSON.stringify(data), {
+       'version': 2
      }).then(function(response, status, data) {
        callback(response, status, data);
      });
